a3c65ed9bcf3be72197ac10b7c68c95c3c7a8721
faisalaltaf <altaffaisal60@gmail.com>
2023-04-18 16:24:14 +0500

    i have created code rest api and table in blade file

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,8 +12,14 @@ class WelcomeController extends Controller
     use LVHTrait;
 
     public function index(){ //home page redirect func
-        
-        return view('home');
+        $shop= auth()->user();
+        $products = $shop->api()->rest('GET','/admin/api/2023-01/products.json'); //shopify product fetch rest api
+        if($products['status'] ==200 || $products['status'] == 201){
+            $products = $products['body']['products'];
+        }else{
+            $products = [];
+        }
+        return view('home',compact('products'));
     }
     public function addProduct(){  //product
         return view("addproduct");
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,77 @@
     </div>
   </div>
 </div>
+<div class="Polaris-Page">
+  <div class="Polaris-Box" style="--pc-box-padding-block-end-xs:var(--p-space-4);--pc-box-padding-block-end-md:var(--p-space-5);--pc-box-padding-block-start-xs:var(--p-space-4);--pc-box-padding-block-start-md:var(--p-space-5);--pc-box-padding-inline-start-xs:var(--p-space-4);--pc-box-padding-inline-start-sm:var(--p-space-0);--pc-box-padding-inline-end-xs:var(--p-space-4);--pc-box-padding-inline-end-sm:var(--p-space-0);position:relative">
+    <div class="Polaris-Page-Header--isSingleRow Polaris-Page-Header--noBreadcrumbs Polaris-Page-Header--mediumTitle">
+      <div class="Polaris-Page-Header__Row">
+        <div class="Polaris-Page-Header__TitleWrapper">
+          <h1 class="Polaris-Header-Title">Sales by product</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="Polaris-LegacyCard">
+      <div class="">
+        <div class="Polaris-DataTable__Navigation">
+          <button class="Polaris-Button Polaris-Button--disabled Polaris-Button--plain Polaris-Button--iconOnly" aria-label="Scroll table left one column" aria-disabled="true" type="button" tabindex="-1">
+            <span class="Polaris-Button__Content">
+              <span class="Polaris-Button__Icon">
+                <span class="Polaris-Icon">
+                  <span class="Polaris-Text--root Polaris-Text--visuallyHidden">
+                  </span>
+                  <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+                    <path d="M12 16a.997.997 0 0 1-.707-.293l-5-5a.999.999 0 0 1 0-1.414l5-5a.999.999 0 1 1 1.414 1.414l-4.293 4.293 4.293 4.293a.999.999 0 0 1-.707 1.707z">
+                    </path>
+                  </svg>
+                </span>
+              </span>
+            </span>
+          </button>
+          <button class="Polaris-Button Polaris-Button--plain Polaris-Button--iconOnly" aria-label="Scroll table right one column" type="button">
+            <span class="Polaris-Button__Content">
+              <span class="Polaris-Button__Icon">
+                <span class="Polaris-Icon">
+                  <span class="Polaris-Text--root Polaris-Text--visuallyHidden">
+                  </span>
+                  <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+                    <path d="M8 16a.999.999 0 0 1-.707-1.707l4.293-4.293-4.293-4.293a.999.999 0 1 1 1.414-1.414l5 5a.999.999 0 0 1 0 1.414l-5 5a.997.997 0 0 1-.707.293z">
+                    </path>
+                  </svg>
+                </span>
+              </span>
+            </span>
+          </button>
+        </div>
+        <div class="Polaris-DataTable Polaris-DataTable__ShowTotals">
+          <div class="Polaris-DataTable__ScrollContainer">
+            <table class="Polaris-DataTable__Table">
+              <thead>
+                <tr>
+                  <th data-polaris-header-cell="true" class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--header" scope="col">id</th>
+                  <th data-polaris-header-cell="true" class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--header Polaris-DataTable__Cell--numeric" scope="col">Title</th>
+                  <th data-polaris-header-cell="true" class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--header Polaris-DataTable__Cell--numeric" scope="col">Status</th>
+                  <th data-polaris-header-cell="true" class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--header Polaris-DataTable__Cell--numeric" scope="col">created_at</th>
+                </tr>
+              </thead>
+              <tbody>
+              @foreach($products as $product)
+                <tr class="Polaris-DataTable__TableRow Polaris-DataTable--hoverable">
+                  <th class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--firstColumn" scope="row">{!!$product->id!!}</th>
+                  <td class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--numeric">{!!$product->title!!}</td>
+                  <td class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--numeric">{!!$product->status!!}</td>
+                  <td class="Polaris-DataTable__Cell Polaris-DataTable__Cell--verticalAlignTop Polaris-DataTable__Cell--numeric">{!!$product->created_at!!}</td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <main class="Polaris-Frame__Main" id="AppFrameMain" data-has-global-ribbon="false">
           <div class="Polaris-Frame__Content">
             <div class="Polaris-Page">
